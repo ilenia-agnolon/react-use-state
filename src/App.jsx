df2c9bf2c67bc3ed94d4bab6b11c5315da1632dd
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-29 16:01:28 +0200

    generata lista di bottoni dinamici da array languages

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,25 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h1>Learn Web development</h1>
+
+      {/* lista bottoni */}
+      <div>
+        {languages.map(({ id, title }) => (
+          <button key={id}>{title}</button>
+        ))}
+      </div>
+
+      {/* testo */}
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit earum
+        mollitia eum sequi? Doloribus in nisi, optio incidunt modi minima alias
+        exercitationem accusamus vero possimus eos vitae reiciendis aut illum?
+      </p>
+    </>
+  );
 }
 
 export default App;
